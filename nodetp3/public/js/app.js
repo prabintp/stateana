aad9a1116eb13544030e4d21c43c099630f4a1ab
prabintp <prabintp@gmail.com>
2013-11-12 00:15:42 +0530

    style and align

--- nodetp3/public/js/app.js
+++ nodetp3/public/js/app.js
@@ -24,3 +24,16 @@ config(function ($routeProvider, $locationProvider) {
 
   $locationProvider.html5Mode(true);
 });
+
+
+$(document).ready( function() {
+	
+	var myData = ["7","4","2","9","8","9","12","12","1","6","5","2","2","4","7","1","4","3","26","6"];
+	var mytitle= ["11 AM","12 PM","1 PM","2 PM","3 PM","4 PM","5 PM","6 PM","7 PM","8 PM","9 PM","10 PM","2 AM","3 AM","4 AM","5 AM","7 AM","9 AM","10 AM","11 AM"];
+    $('#tpHourVisitBar').tpgraph({
+       ydata       : myData,
+       xdata       : mytitle
+      
+	});
+
+});
