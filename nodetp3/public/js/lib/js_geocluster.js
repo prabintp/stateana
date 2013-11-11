f87ef5f14dd780fed60de7f445078f66eab3a3f6
prabintp <prabintp@gmail.com>
2013-11-11 18:21:49 +0530

    cluster geo amp

---
+++ nodetp3/public/js/lib/js_geocluster.js
@@ -0,0 +1,33 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*
+var data1='../geocluster_data';
+console.log(data1);
+
+function initialize() {
+        var center = new google.maps.LatLng(37.4419, -122.1419);
+
+        var map = new google.maps.Map(document.getElementById('geocluster_map'), {
+          zoom: 3,
+          center: center,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+
+        var markers = [];
+        for (var i = 0; i < 100; i++) {
+          var dataPhoto = data1[i];
+          var latLng = new google.maps.LatLng(dataPhoto.latitude,
+              dataPhoto.longitude);
+          var marker = new google.maps.Marker({
+            position: latLng
+          });
+          markers.push(marker);
+        }
+        var markerCluster = new MarkerClusterer(map, markers);
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
+
+
+*/
